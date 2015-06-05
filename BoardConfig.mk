3e28847b0b20d202acabf6348c2279cd83ce3d50
alfsamsung <alfsamsung@hotmail.com>
2015-06-05 16:56:15 +0200

    Gcc: Change kernel Gcc to ArchiToolchain 5.1. Thank you JustArchi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,7 @@ TARGET_GCC_VERSION_EXP := 4.8-sm
 # This is needed for GCC 4.9
 #COMMON_GLOBAL_CFLAGS += -Wno-maybe-uninitialized
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.1-ub/bin/
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.1-ar/bin/
 # A few kernel gcc config options 
 export CONFIG_GRAPHITE_FLAGS := true
 #export CONFIG_CC_OPTIMIZE_MORE := true
