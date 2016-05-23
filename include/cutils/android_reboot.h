75100a15f715783f877756863d950d02b39b59b0
alfsamsung <alfsamsung@hotmail.com>
2016-05-23 20:15:50 +0200

    fix reboot to recovery again after code change in libutils.
    Change-Id: I1d559c0f0d236f46b9c540e9e85449f63703095a

--- include/cutils/android_reboot.h
+++ include/cutils/android_reboot.h
@@ -15,7 +15,6 @@
  */
 
 #include "../../../../../system/core/include/cutils/android_reboot.h"
-#include <stdlib.h>
 
 /* Inject code only in android_reboot.c */
 #ifdef _SYS_REBOOT_H_
@@ -23,18 +22,22 @@
 #ifdef _SYS_TYPES_H_
 #ifdef _SYS_STAT_H_
 
-int android_reboot2(int cmd, int flags, const char *arg);
+int android_reboot_with_callback2(
+    int cmd, int flags, const char *arg,
+    void (*cb_on_remount)(const struct mntent*));
 
-int android_reboot(int cmd, int flags __attribute__((unused)), const char *arg)
+int android_reboot_with_callback(
+    int cmd, int flags __unused, const char *arg,
+    void (*cb_on_remount)(const struct mntent*))
 {
     if (cmd == (int) ANDROID_RB_RESTART2 && arg && strlen(arg) > 0) {
         char cmd[PATH_MAX];
         sprintf(cmd, "/system/bin/setup-recovery" " %s", arg);
         system(cmd);
     }
-    return android_reboot2(cmd, flags, arg);
+    return android_reboot_with_callback2(cmd, flags, arg, cb_on_remount);
 }
-#define android_reboot(cmd, flags, arg) android_reboot2(cmd, flags, arg)
+#define android_reboot_with_callback(cmd, flags, arg, cb_on_remount) android_reboot_with_callback2(cmd, flags, arg, cb_on_remount)
 #endif /* _SYS_STAT_H_ */
 #endif /* _SYS_TYPES_H_ */
 #endif /* _SYS_SYSCALL_H_ */
